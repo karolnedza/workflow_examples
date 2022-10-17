28add3a45286e4e4c314c94375fbfa23acdfe4f2
Nathan Manville <nmanville@aviatrix.com>
2022-10-17 13:54:53 -0700

    Remove vpc_ from input vars

--- wf_templates/deploy_avx_spoke_gw/main.tf
+++ wf_templates/deploy_avx_spoke_gw/main.tf
@@ -1,10 +1,10 @@
 module "mc-spoke" {
   source  = "../modules/mc-spoke"
-  name = var.vpc_name
+  name = var.name
   cloud = var.cloud[var.region]
   region = var.region
   account = var.account_name[var.region]
-  cidr = var.vpc_cidr
+  cidr = var.cidr
   transit_gw = var.transit_gw[var.region]
   network_domain = var.network_domain
   ha_gw = false
